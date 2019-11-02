414d3150e7999ca715033cb13e3189d23d47e6dd
Samatbek89 <sambrero899@gmail.com>
2019-11-02 23:01:02 +0600

    измененное

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,24 +9,11 @@ public class Main {
         names[1] = "Dasha";
         names[2] = "Kuba";
 
-        System.out.println("Доброе утро"  +  names [0] + " " + "Добрый день" + names[1] + " " + "Добрый  вечер" + names[2]);
 
-
-        String name = "Nurik";
-        switch (name) {
-            case "Roma":
-                System.out.println("Доброе утро Петя" );
-                break;
-            case "Dasha":
-                System.out.println("Добрый день Даша");
-                break;
-            case "Maksat":
-                System.out.println("Добрый вечер Куба");
-                break;
-            default:
-                System.out.println("Привет всем");
-
-
+        int name1 = 5;
+        while (name1 <= 0);{
+            System.out.println("Доброе утро " + names[0] + " " + "Добрый день " + names[1] + " " + "Добрый вечер" + names[2]);
+            name1++;
 
         }
 
@@ -34,6 +21,8 @@ public class Main {
 
 
 
-    }
 
+
+
+    }
 }
